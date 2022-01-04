8a4be64d3efd1dab8f7cd11f1327a31bad09a3bc
Natalya17041986 <natabavyka@gmail.com>
2022-01-04 18:07:34 +0300

    v5

--- project_0/game3.py
+++ project_0/game3.py
@@ -1,21 +1,6 @@
 """Игра: Угадай число за меньшее количество попыток"""
 
-
 import numpy as np
-
-def random_predict(number: int = 1) -> int:
-
-    count = 0
-
-    while True:
-        count += 1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
-        if number == predict_number:
-            break  # выход из цикла, если угадали
-    return count
-
-
-print(f"Количество попыток: {random_predict()}")
 
 def game_core(number: int = 1) -> int:
     
