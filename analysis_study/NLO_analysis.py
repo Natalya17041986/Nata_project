8037d345f41f86963e6d24c789bbeeea9c3e8b28
Natalya17041986 <natabavyka@gmail.com>
2022-01-29 17:45:06 +0300

    q2

--- analysis_study/NLO_analysis.py
+++ analysis_study/NLO_analysis.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
 #В каком году отмечается наибольшее количество случаев наблюдения НЛО в США?
-df = pd.read_csv('data/NLO.csv', sep=',')
+def new_func():
+    df = pd.read_csv('data/NLO.csv', sep=',')
+    return df
+
+df = new_func()
 df['Time'] = pd.to_datetime(df.Time)
 print(df['Time'].dt.year.mode()[0])
 
